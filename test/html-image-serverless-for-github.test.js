13da66476ab67aaa6fb6716aa6b1378d572c0ecb
HyunggiPark <phg9587@naver.com>
2024-03-26 17:04:38 +0900

    Initial commit

---
+++ test/html-image-serverless-for-github.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const HtmlImageServerlessForGithub = require('../lib/html-image-serverless-for-github-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/html-image-serverless-for-github-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new HtmlImageServerlessForGithub.HtmlImageServerlessForGithubStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
